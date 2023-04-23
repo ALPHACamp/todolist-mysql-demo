8ddd1447063b7712287fd3bd8a11ba002861e77d
ShangWei Huang <shangwei.huang@iherb.com>
2023-04-23 15:30:33 +0800

    router init

--- app.js
+++ app.js
@@ -6,6 +6,34 @@ app.get('/', (req, res) => {
 	res.send('hello world')
 })
 
+app.get('/todos', (req, res) => {
+	res.send('get all todos')
+})
+
+app.get('/todos/new', (req, res) => {
+	res.send('get todo form')
+})
+
+app.post('/todos', (req, res) => {
+	res.send('add todo')
+})
+
+app.get('/todos/:id', (req, res) => {
+	res.send(`get todo: ${req.params.id}`)
+})
+
+app.get('/todos/:id/edit', (req, res) => {
+	res.send(`get todo edit form, id: ${req.params.id}`)
+})
+
+app.put('/todos/:id', (req, res) => {
+	res.send(`todo id: ${req.params.id} has been modified`)
+})
+
+app.delete('/todos/:id', (req, res) => {
+	res.send(`todo id: ${req.params.id} has been deleted`)
+})
+
 app.listen(port, () => {
 	console.log(`App is running on http://localhost:${port}`)
 })
